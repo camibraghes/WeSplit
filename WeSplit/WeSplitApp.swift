0eb0b86461168d7a8ca462a7f498242f52f61274
Camelia Braghes <cameliabraghes@Camelias-MacBook-Pro.local>
2022-11-07 10:20:11 +0200

    Initial setup

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Camelia Braghes on 07.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
